760a9eccedf800ba701b7a2e0ba82a9d961121f5
arturooreaf <aoreaf@gmail.com>
2026-07-20 10:45:47 +0200

    feat: add Read Peliculas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 app.use(express.json()); //leer el json
+const db = require('./models/db');
 const PORT = 3000;
 
 // Tu primera ruta (El mostrador de recepción)
 app.get('/', (req, res) => {
     res.send('¡El servidor de películas está funcionando a la perfección!');
+});
+//Ruta para obtener todas las películas
+//READ 
+app.get('/peliculas', async (req, res) => {
+    try {
+        const resultado = await db.query('SELECT * FROM peliculas');
+        res.json(resultado.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error al obtener las películas' });
+    }
 });
 
 // Le decimos al servidor que se quede escuchando
